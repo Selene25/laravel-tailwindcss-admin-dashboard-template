04d401bd6e144c1f04d4ea1bd9ee28db5cfbe6ab
Selene25 <veinangeles25@gmail.com>
2025-03-22 06:12:24 +0800

    login and register

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -28,46 +28,32 @@
             }
         </script>
     </head>
+
     <body class="font-inter antialiased bg-gray-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400">
-
         <main class="bg-white dark:bg-gray-900">
-
-            <div class="relative flex">
-
-                <!-- Content -->
-                <div class="w-full md:w-1/2">
-
-                    <div class="min-h-[100dvh] h-full flex flex-col after:flex-1">
-
-                        <!-- Header -->
-                        <div class="flex-1">
-                            <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
-                                <!-- Logo -->
-                                <a class="block" href="{{ route('dashboard') }}">
-                                    <svg class="fill-violet-500" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-                                        <path d="M31.956 14.8C31.372 6.92 25.08.628 17.2.044V5.76a9.04 9.04 0 0 0 9.04 9.04h5.716ZM14.8 26.24v5.716C6.92 31.372.63 25.08.044 17.2H5.76a9.04 9.04 0 0 1 9.04 9.04Zm11.44-9.04h5.716c-.584 7.88-6.876 14.172-14.756 14.756V26.24a9.04 9.04 0 0 1 9.04-9.04ZM.044 14.8C.63 6.92 6.92.628 14.8.044V5.76a9.04 9.04 0 0 1-9.04 9.04H.044Z" />
-                                    </svg> 
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="max-w-sm mx-auto w-full px-4 py-8">
-                            {{ $slot }}
-                        </div>
-
+            <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900 relative">
+                <img id="background" class="absolute inset-0 w-full h-full object-cover" src="{{ asset('images/bg.png') }}" alt="Laravel background" />
+            
+                    <!-- Header -->
+                    <div class="relative z-10">
+                        <a class="block" href="{{ route('dashboard') }}">
+                            <img src="{{ asset('images/z.png') }}" alt="logo" class="w-12 h-12">
+                        </a>
                     </div>
 
+                    @if (Route::currentRouteName() == 'register')
+                        <div class="relative z-10 w-full max-w-4xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                            {{ $slot }}
+                        </div>
+                    @elseif (Route::currentRouteName() == 'login')
+                        <div class="relative z-10 w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                            {{ $slot }}
+                        </div>
+                    @else
+                        {{ $slot }}
+                    @endif
                 </div>
-
-                <!-- Image -->
-                <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
-                    <img class="object-cover object-center w-full h-full" src="{{ asset('images/auth-image.jpg') }}" width="760" height="1024" alt="Authentication image" />
-                </div>
-
-            </div>
-
-        </main> 
-
+        </main>
         @livewireScriptConfig
     </body>
 </html>
