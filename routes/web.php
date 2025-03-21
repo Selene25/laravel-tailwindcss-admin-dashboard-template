2813e7c9a1abf134bd837defb247b8420bf41227
Selene25 <veinangeles25@gmail.com>
2025-03-21 19:47:23 +0800

    bb

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataFeedController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\InvoiceController;
-use App\Http\Controllers\MemberController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\JobController;
-use App\Http\Controllers\CampaignController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,81 +22,12 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     // Route for the getting the data feed
     Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/dashboard/analytics', [DashboardController::class, 'analytics'])->name('analytics');
-    Route::get('/dashboard/fintech', [DashboardController::class, 'fintech'])->name('fintech');
-    Route::get('/ecommerce/customers', [CustomerController::class, 'index'])->name('customers');
-    Route::get('/ecommerce/orders', [OrderController::class, 'index'])->name('orders');
-    Route::get('/ecommerce/invoices', [InvoiceController::class, 'index'])->name('invoices');
-    Route::get('/ecommerce/shop', function () {
-        return view('pages/ecommerce/shop');
-    })->name('shop');    
-    Route::get('/ecommerce/shop-2', function () {
-        return view('pages/ecommerce/shop-2');
-    })->name('shop-2');     
-    Route::get('/ecommerce/product', function () {
-        return view('pages/ecommerce/product');
-    })->name('product');
-    Route::get('/ecommerce/cart', function () {
-        return view('pages/ecommerce/cart');
-    })->name('cart');    
-    Route::get('/ecommerce/cart-2', function () {
-        return view('pages/ecommerce/cart-2');
-    })->name('cart-2');    
-    Route::get('/ecommerce/cart-3', function () {
-        return view('pages/ecommerce/cart-3');
-    })->name('cart-3');    
-    Route::get('/ecommerce/pay', function () {
-        return view('pages/ecommerce/pay');
-    })->name('pay');     
-    Route::get('/campaigns', [CampaignController::class, 'index'])->name('campaigns');
-    Route::get('/community/users-tabs', [MemberController::class, 'indexTabs'])->name('users-tabs');
-    Route::get('/community/users-tiles', [MemberController::class, 'indexTiles'])->name('users-tiles');
-    Route::get('/community/profile', function () {
-        return view('pages/community/profile');
-    })->name('profile');
-    Route::get('/community/feed', function () {
-        return view('pages/community/feed');
-    })->name('feed');     
-    Route::get('/community/forum', function () {
-        return view('pages/community/forum');
-    })->name('forum');
-    Route::get('/community/forum-post', function () {
-        return view('pages/community/forum-post');
-    })->name('forum-post');    
-    Route::get('/community/meetups', function () {
-        return view('pages/community/meetups');
-    })->name('meetups');    
-    Route::get('/community/meetups-post', function () {
-        return view('pages/community/meetups-post');
-    })->name('meetups-post');    
-    Route::get('/finance/cards', function () {
-        return view('pages/finance/credit-cards');
-    })->name('credit-cards');
-    Route::get('/finance/transactions', [TransactionController::class, 'index01'])->name('transactions');
-    Route::get('/finance/transaction-details', [TransactionController::class, 'index02'])->name('transaction-details');
-    Route::get('/job/job-listing', [JobController::class, 'index'])->name('job-listing');
-    Route::get('/job/job-post', function () {
-        return view('pages/job/job-post');
-    })->name('job-post');    
-    Route::get('/job/company-profile', function () {
-        return view('pages/job/company-profile');
-    })->name('company-profile');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');  
+    
     Route::get('/messages', function () {
         return view('pages/messages');
     })->name('messages');
-    Route::get('/tasks/kanban', function () {
-        return view('pages/tasks/tasks-kanban');
-    })->name('tasks-kanban');
-    Route::get('/tasks/list', function () {
-        return view('pages/tasks/tasks-list');
-    })->name('tasks-list');       
-    Route::get('/inbox', function () {
-        return view('pages/inbox');
-    })->name('inbox'); 
-    Route::get('/calendar', function () {
-        return view('pages/calendar');
-    })->name('calendar'); 
+
     Route::get('/settings/account', function () {
         return view('pages/settings/account');
     })->name('account');  
@@ -122,36 +46,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/settings/feedback', function () {
         return view('pages/settings/feedback');
     })->name('feedback');
-    Route::get('/utility/changelog', function () {
-        return view('pages/utility/changelog');
-    })->name('changelog');  
-    Route::get('/utility/roadmap', function () {
-        return view('pages/utility/roadmap');
-    })->name('roadmap');  
-    Route::get('/utility/faqs', function () {
-        return view('pages/utility/faqs');
-    })->name('faqs');  
-    Route::get('/utility/empty-state', function () {
-        return view('pages/utility/empty-state');
-    })->name('empty-state');  
-    Route::get('/utility/404', function () {
-        return view('pages/utility/404');
-    })->name('404');
-    Route::get('/utility/knowledge-base', function () {
-        return view('pages/utility/knowledge-base');
-    })->name('knowledge-base');
-    Route::get('/onboarding-01', function () {
-        return view('pages/onboarding-01');
-    })->name('onboarding-01');   
-    Route::get('/onboarding-02', function () {
-        return view('pages/onboarding-02');
-    })->name('onboarding-02');   
-    Route::get('/onboarding-03', function () {
-        return view('pages/onboarding-03');
-    })->name('onboarding-03');   
-    Route::get('/onboarding-04', function () {
-        return view('pages/onboarding-04');
-    })->name('onboarding-04');
+
     Route::get('/component/button', function () {
         return view('pages/component/button-page');
     })->name('button-page');
